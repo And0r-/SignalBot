4af02573e3ed381f27e7b8ada793ad80f08d80fb
And0r <ahabegger@gmail.com>
2019-04-18 17:30:01 +0200

    refactoring service start. fork a fork is a littlebit suboptimal :D create 2 different services in init.d

--- start.pl
+++ start.pl
@@ -4,16 +4,15 @@
 ##################################################################################################################################
 ##
 ##	This will start the Signal Bot
-##	Use no param to start it with forks in background
+##	Use no param to start it with fork in background
 ##
-##	Or You can use a param in development
-##	1 => starts the signal client listener with no fork
+##	you have to set the service to start "signalBot" or "timer".
+##  for all functions you will need all services.
+##
+##	the second param you can set for debug mode. So the service will be startet with no fork
 ##
 ##	You can set "has signal_cli => 'debug';" in config file.
 ##	so you can input signal messages in signal_input.txt and read outputs in terminal. 
-##
-##
-##	2 => starts the bot background worker to detect starting events and do all the stuff that is not signal message triggered
 ##
 ##
 ##
@@ -31,24 +30,27 @@ use File::Pid;
 
 use SignalBot;
 
-
+my $service = shift;
 my $debug = shift;
 
-if ($debug && $debug == 1) {
+my $pidFiles = {
+	signalBot => ".signalBot.pid",
+	timer => ".signalBotTimer.pid"
+};
+
+die "service nicht definiert signalBot oder timer?" unless ($service);
+
+if ($debug && $service eq "signalBot") {
 	# do not fork and output all stuff in terminal
 	SignalBot->new->init->signal_cli->StartReactor;
 	exit;
 }
 
-
-if ($debug && $debug == 2) {
+if ($debug && $service eq "timer") {
 	# do not fork and output all stuff in terminal
 	SignalBot->new->init->timer->start;
 	exit;
 }
-
-my $pidFile       = ".signalBot.pid";
-my $pidFileTimer       = ".timer.pid";
 
 # daemonize
 use POSIX qw(setsid);
@@ -59,7 +61,6 @@ open STDOUT, '>>/dev/null' or die "Can't write to /dev/null: $!";
 # open STDERR, '>>/dev/null' or die "Can't write to /dev/null: $!"; # Temporary disabled to debug
 defined( my $pid = fork ) or die "Can't fork: $!";
 exit if $pid;
-defined( my $pid2 = fork ) or die "Can't fork: $!";
 
 # dissociate this process from the controlling terminal that started it and stop being part
 # of whatever process group this process was a part of.
@@ -69,41 +70,28 @@ POSIX::setsid() or die "Can't start a new session.";
 $SIG{INT} = $SIG{TERM} = $SIG{HUP} = \&signalHandler;
 $SIG{PIPE} = 'ignore';
  
-my $pidfile = undef;
-my $pidfileTimer = undef;
 my $SignalBot;
 
+my $pidfile = File::Pid->new( { file => $pidFiles->{$service}, } );
+$pidfile->write or die "Can't write PID file, /dev/null: $!";
 
-if ($pid2) {
-	# fork 2
+if ($service eq "signalBot") {
+	# listen everytime for signal messages
+	$SignalBot = SignalBot->new->init;
+	$SignalBot->signal_cli->StartReactor;
+} elsif ($service eq "timer") {
 	# do time based stuff. e.g check is starting a event
-	$pidfileTimer = File::Pid->new( { file => $pidFileTimer, } );
-	 
-	$pidfileTimer->write or die "Can't write PID file, /dev/null: $!";
-
 	$SignalBot = SignalBot->new->init;
 	$SignalBot->timer->start;
-} else {
-	# fork 1
-	# listen everytime for signal messages
-	# create pid file in /var/run/
-	$pidfile = File::Pid->new( { file => $pidFile, } );
-	 
-	$pidfile->write or die "Can't write PID file, /dev/null: $!";
-
-	 $SignalBot = SignalBot->new->init;
-	$SignalBot->signal_cli->StartReactor;
 }
-
-
 
 
 # catch signals and end the program if one is caught.
 sub signalHandler {
-	if ($pid2) {
-		$SignalBot->timer->stop;
-	} else {
-		$SignalBot->signal_cli->StopReactor;    # this will cause the "infinite loop" to exit
+	if ($service eq "signalBot") {
+		$SignalBot->signal_cli->StopReactor; # this will cause the "infinite loop" to exit
+	} elsif ($service eq "timer") {
+		$SignalBot->timer->stop; # this will cause the "infinite loop" to exit
 	}
 }
  
@@ -111,5 +99,4 @@ sub signalHandler {
  # do this stuff when exit() is called.
 END {
 	$pidfile->remove if defined $pidfile;
-	$pidfileTimer->remove if defined $pidfileTimer;
 }
